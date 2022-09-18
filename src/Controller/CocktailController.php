446246106b162e9912a45eb4615aa1c1bf5f0f1d
Mantas <mantas.smulkstys@gmail.com>
2022-09-18 15:52:53 +0300

    created separate controller method for displaying form input and result

--- src/Controller/CocktailController.php
+++ src/Controller/CocktailController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use App\Form\CocktailFormType;
+use App\Form\CocktailType;
 use App\Entity\CocktailName;
+use phpDocumentor\Reflection\Types\String_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,25 +12,49 @@ use Symfony\Component\HttpFoundation\Request;
 
 class CocktailController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
-    public function new(Request $request): Response
+    #[Route('/', name: 'initForm')]
+    public function initForm()
     {
         $cocktailName = new CocktailName();
-
-        $form = $this->createForm(CocktailFormType::class, $cocktailName);
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()){
-
-            $cocktailName = $form->getData();
-
-        }
+        $form = $this->createForm(CocktailType::class, $cocktailName, [
+            'action' => $this->generateUrl('submitName'),
+        ]);
 
         return $this->render('cocktail/home.html.twig', [
             'cocktailForm' => $form->createView(),
-            'cocktailName' => $cocktailName
+            'cocktailName' => '',
+            'errorMessage' => ''
         ]);
-
     }
 
+    #[Route('/submit', name: 'submitName')]
+    public function submitName(Request $request): Response
+    {
+        $cocktailName = new CocktailName();
+        $form = $this->createForm(CocktailType::class, $cocktailName);
+
+        $form->handleRequest($request);
+
+        //TODO: add validations so only string or numstring would be preferred.
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->render('cocktail/submit.html.twig', [
+                'cocktailForm' => $form,
+                'result' => 'Error on submitting value'
+            ]);
+        }
+
+        //TODO read about phpDoc
+
+        /** @var CocktailName $cocktailNameEntity */
+        $cocktailNameEntity = $form->getData();
+
+        $cocktailName->setCocktailName($cocktailNameEntity->getCocktailName());
+
+        //TODO API request action
+
+        return $this->render('cocktail/submit.html.twig', [
+            'cocktailForm' => $form->createView(),
+            'result' => $cocktailName->getCocktailName()
+        ]);
+    }
 }
